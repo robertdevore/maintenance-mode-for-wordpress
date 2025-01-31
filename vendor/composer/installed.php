768b345aaaf93e2fca21f3100554981e3a8ea9bf
Robert DeVore <deviodigital@gmail.com>
2025-01-31 10:35:11 -0500

    :ok_hand: IMPROVE: Updated plugin with WPCom Check

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '44c5b9452d9d46268c8b3cc8946f9427aaf3f308',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '44c5b9452d9d46268c8b3cc8946f9427aaf3f308',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'robertdevore/wpcom-check' => array(
+            'pretty_version' => '1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'f4c4052c2ae53218b561606f3801afdf3a5aa899',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../robertdevore/wpcom-check',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
